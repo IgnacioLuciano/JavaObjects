d5d3c46f3275adf8d8415f87b333d7e9f5133376
Unknown <iglumu@gmail.com>
2018-01-09 20:49:58 -0800

    HandsOn

---
+++ com.IgnacioLuciano.java/src/com/IgnacioLuciano/java/PersonDetails.java
@@ -0,0 +1,13 @@
+package com.IgnacioLuciano.java;
+
+public class PersonDetails {
+	
+	thePersonDetails = new thePersonDetails() {
+
+
+    String firstName = "Ignacio";
+    String lastName = "Luciano";
+    String age = "37";
+    System.out.println(firstName + " " + lastName + " I am " + " " + age);
+}
+}
