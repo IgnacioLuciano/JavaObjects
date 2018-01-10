7df81853294899ceee4e14542380e538a78a878f
Unknown <iglumu@gmail.com>
2018-01-10 15:50:37 -0800

    Exercise

--- com.IgnacioLuciano.java/src/com/IgnacioLuciano/java/Person.java
+++ com.IgnacioLuciano.java/src/com/IgnacioLuciano/java/Person.java
@@ -1,49 +1,34 @@
 package com.IgnacioLuciano.java;
 
 public class Person {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String firstName;
-		String lastName;
-		String age;
-
-	new Person(){
-		String firstName = "";
-		String lastName = "";
-		String age = "";
+	String firstName;
+	String lastName;
+	String age;
 
 	
-	public void Person(String first, String last, String yourage) {
-		String firstName = first;
-		String lastName = last;
-		String age = yourage;
-	}
-
-	public String getFirstName(){
+	public String getFirstName() {
 		return firstName;
-	
+
 	}
-	public void setFirstName(String newFirstName){
+
+	public void setFirstName(String newFirstName) {
 		firstName = newFirstName;
 	}
 
-	public String getLastName(){
+	public String getLastName() {
 		return lastName;
-	
+
 	}
-	public void setLastName(String newLastName){
+
+	public void setLastName(String newLastName) {
 		lastName = newLastName;
 	}
 
-	public String getAge(){
+	public String getAge() {
 		return age;
 	}
 
-	public void setAge(String newAge){
+	public void setAge(String newAge) {
 		age = newAge;
 	}
 }
-}
-}
-
---
+++ com.IgnacioLuciano.java/src/com/IgnacioLuciano/java/StartUp.java
@@ -0,0 +1,13 @@
+package com.IgnacioLuciano.java;
+
+public class StartUp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	Person thePerson = new Person();
+	thePerson.setFirstName("Ignacio");
+	String theFirstName = thePerson.getFirstName();
+	System.out.println(theFirstName);
+	}
+}
